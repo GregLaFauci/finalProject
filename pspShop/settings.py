9dd2db4ea0a3e02ba5c80aafd48f4441e65a1476
Greg LaFauci <greg.lafauci@codeimmersives.com>
2020-07-07 07:23:47 -0400

    braintree payment gateway configured

--- pspShop/settings.py
+++ pspShop/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
+    'payment.apps.PaymentConfig',
 
 ]
 
@@ -143,3 +144,17 @@ CART_SESSION_ID = 'cart'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
+# Braintree settings
+
+BRAINTREE_MERCHANT_ID = os.getenv("BRAINTREE_MERCHANT_ID")  # Merchant ID
+BRAINTREE_PUBLIC_KEY = os.getenv("BRAINTREE_PUBLIC_KEY")   # Public Key
+BRAINTREE_PRIVATE_KEY = os.getenv("BRAINTREE_PRIVATE_KEY")  # Private key
+
+import braintree
+
+BRAINTREE_CONF = braintree.Configuration(
+    braintree.Environment.Sandbox,
+    BRAINTREE_MERCHANT_ID,
+    BRAINTREE_PUBLIC_KEY,
+    BRAINTREE_PRIVATE_KEY
+)
