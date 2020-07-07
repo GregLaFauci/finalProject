05f4d3719c9c67da55ae40f9a0ae60a37eb32638
Greg LaFauci <greg.lafauci@codeimmersives.com>
2020-07-07 10:01:45 -0400

    redis recommender

--- shop/recommender.py
+++ shop/recommender.py
@@ -1,10 +1,13 @@
 import redis
 from django.conf import settings
 from .models import Product
+
 # connect to redis
 r = redis.Redis(host=settings.REDIS_HOST,
                 port=settings.REDIS_PORT,
                 db=settings.REDIS_DB)
+print(r)
+                
 class Recommender(object):
     def get_product_key(self, id):
         return f'product:{id}:purchased_with'
